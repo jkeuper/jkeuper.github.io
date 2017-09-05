4cd6560cb81ed11c8c2628bdc09e7d2ac87c7109
Jasper <jkeuper@gmail.com>
2017-09-05 22:14:15 +0200

    Quest for fixing search...

--- js/search.js
+++ js/search.js
@@ -57,7 +57,7 @@
     
     //var results = idx.search(searchTerm); // Get lunr to perform a search
     var results = idx.query(function (q) {
-                            q.term("*" + searchTerm + "*", []})
+                            q.term("*" + searchTerm + "*")
                           });
     displaySearchResults(results, window.store); // We'll write this in the next section
   }
