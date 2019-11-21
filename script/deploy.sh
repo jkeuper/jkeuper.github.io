b08237653ae071a9ebe9c407c0fadd54adb4a89a
Jasper Keuper <jkeuper@gmail.com>
2019-11-21 22:24:55 +0100

    Fixing deploy

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
-s
-et -e # halt script on error
 
-if [ "x$SFTP_USER$SFTP_PASS$SFTP_URL" != "x" ]; then
+set -e # halt script on error
+
+if [ "x$SFTP_USER$SFTP_PASS$SFTP_URL" = "x" ]; then
+   exit
+fi
 
 sshpass -p '$SFTP_PASS' sftp -o BatchMode=no -oStrictHostKeyChecking=no -b - $SFTP_USER@$SFTP_URL << !
    cd www
@@ -11,4 +13,3 @@ sshpass -p '$SFTP_PASS' sftp -o BatchMode=no -oStrictHostKeyChecking=no -b - $SF
    bye
 !
 
-fi
